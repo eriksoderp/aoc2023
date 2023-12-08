7b2a9bfe727bfdd78e999d3ca88d7e5d235fccde
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2023-12-08 20:36:35 +0100

    Update day8.py

--- day8.py
+++ day8.py
@@ -9,7 +9,7 @@ words = lambda line: re.findall(r'[A-Z]+', line)
 transitions = {node:(left, right) for node, left, right in map(words, lines[2:])}
 instructions = [0 if c == 'L' else 1 for c in lines[0].strip()]
 
-def rounds_steps(node, stop_condition):
+def steps_rounds(node, stop_condition):
     steps = 0
     while not stop_condition(node):
         lr = instructions[steps % len(instructions)]
@@ -18,10 +18,10 @@ def rounds_steps(node, stop_condition):
     return steps, steps // len(instructions)
 
 # part 1
-print(rounds_steps('AAA', lambda node: node == 'ZZZ')[0])
+print(steps_rounds('AAA', lambda node: node == 'ZZZ')[0])
 
 # part 2
 ends_with_a = list(filter(lambda node: node.endswith('A'), transitions.keys()))
 z_funcs = itertools.repeat(lambda node: node.endswith('Z'), len(ends_with_a))
-res = list(map(rounds_steps, ends_with_a, z_funcs))
+res = list(map(steps_rounds, ends_with_a, z_funcs))
 print(lcm(*list(map(lambda x: int(x[1]), res)))*len(instructions))
